675d4d48dd965dc8b765162c5d37464cb17aab15
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-03-29 20:20:23 +0200

    Common function for searching stations

--- src/pollution.erl
+++ src/pollution.erl
@@ -4,13 +4,15 @@
 -include("../include/pollution.hrl").
 
 %%-type sensor_name() :: string().
+-type id() :: {name, station_name()} | {coord, coord()}.
 
 -export_type([station_name/0]).
 
 
 %% API
-%%-export([createMonitor/0]).
 -export([addStation/3, addValue/5, removeValue/4,getOneValue/4, getStationMean/3, getDailyMean/3]).
+
+
 -compile(export_all).
 
 createMonitor() ->
@@ -18,29 +20,17 @@ createMonitor() ->
 
 
 -spec addStation(Name :: station_name(), Coord :: coord(), #monitor{}) -> #monitor{}.
-addStation(Name, Coord, Monitor) ->
+addStation(Name, Coord, M) ->
     #monitor{coord_to_name = CtN, name_to_senor = NtS} = Monitor,
-    case {findByCoord(Coord, Monitor), findByName(Name, Monitor)}  of
+    case {findStation({name, Name}, M), findStation({coord,Coord}, M)}  of
         {{error, not_found}, {error, not_found}} ->
-            Monitor#monitor{
+            M#monitor{
                 coord_to_name = CtN#{Coord => Name},
                 name_to_senor = CtN#{Name => #station{
                     coord = Coord, name = Name, data = []
                 }}
             };
         _ -> {error, exists}
-    end.
-
-
-findByName(Name, #monitor{name_to_senor = NtS}) ->
-    maps:get(Name, NtS, {error, not_found}).
-
-
--spec findByCoord(coord(), #monitor{}) -> #station{} | {error, not_found}.
-findByCoord(Coord, #monitor{coord_to_name = CtN, name_to_senor = NtS}) ->
-    case maps:get(Coord, CtN, undefined) of
-        undefined -> {error, not_found};
-        Name -> maps:get(Name, NtS)
     end.
 
 
@@ -65,3 +55,13 @@ getStationMean(_Arg0, _Arg1, _Arg2) ->
 % typ, dzien
 getDailyMean(_Arg0, _Arg1, _Arg2) ->
     erlang:error(not_implemented).
+
+
+-spec findStation(id(), #monitor{}) -> #station{} | {error, not_found}.
+findStation({name, Name}, #monitor{name_to_senor = NtS}) ->
+    maps:get(Name, NtS, {error, not_found});
+findStation({coord, Coord}, #monitor{coord_to_name = CtN, name_to_senor = NtS}) ->
+    case maps:get(Coord, CtN, undefined) of
+        undefined -> {error, not_found};
+        Name -> maps:get(Name, NtS)
+    end.

031364d90e03cd3230d56db0f0ff725eee5724dd
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-04-12 08:57:02 +0200

    Implement daily mean and air quality index

--- src/pollution.erl
+++ src/pollution.erl
@@ -8,11 +8,14 @@
 
 -export_type([station_name/0]).
 
--export([createMonitor/0, addStation/3, addValue/5, removeValue/4,getOneValue/4, getStationMean/3,
-    getDailyMean/3]).
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4,getOneValue/4,
+    getStationMean/3, getDailyMean/3, getAirQualityIndex/3]).
 
 
-%%-compile(export_all).
+
+%%%===================================================================
+%%% Public API
+%%%===================================================================
 
 -spec createMonitor() -> monitor().
 createMonitor() ->
@@ -20,29 +23,29 @@ createMonitor() ->
 
 
 -spec addStation(Name :: station_name(), Coord :: coord(), monitor()) ->
-    {ok, monitor()} | {error, exists}.
+    monitor() | {error, exists}.
 addStation(Name, Coord, M) ->
     #monitor{coord_to_name = CtN, name_to_station = NtS} = M,
-    case {findStation({name, Name}, M), findStation({coord,Coord}, M)}  of
-        {{error, not_found}, {error, not_found}} ->
+    case isStationAvailable(Name, Coord, M) of
+        true ->
             Station = #station{coord = Coord, name = Name},
-            {ok, M#monitor{
+            M#monitor{
                 coord_to_name = CtN#{Coord => Name},
                 name_to_station = NtS#{Name => Station}
-            }};
-        _ -> {error, exists}
+            };
+        false -> {error, exists}
     end.
 
 
 -spec addValue(id(), timestamp(), kind(), float(), monitor()) ->
-    {ok, monitor()} | {error, exists}.
+    monitor() | {error, exists}.
 addValue(CoordOrName, Datetime, MeasureKind, Value, M) ->
     {ok, #station{data = Data} = S} = findStation(CoordOrName, M),
     case exists(Datetime, MeasureKind, Data) of
         true -> {error, exists};
         false ->
             Point = buildPoint(Datetime, MeasureKind, Value),
-            {ok, updateStation(S#station{data = [Point | Data]}, M)}
+            updateStation(S#station{data = [Point | Data]}, M)
     end.
 
 
@@ -70,17 +73,62 @@ getOneValue(CoordOrName, Time, Kind, M) ->
     end.
 
 
--spec getStationMean(id(), kind(), monitor()) -> float().
+-spec getStationMean(id(), kind(), monitor()) -> float() | undefined.
 getStationMean(CoordOrName, Type, M) ->
     {ok, #station{data = Data}} = findStation(CoordOrName, M),
-    FilteredData = lists:filter(fun({_, T, _}) -> T =:= Type end, Data),
-    lists:sum(lists:map(fun({_, _, Value}) -> Value end, FilteredData))
-        / length(FilteredData).
+    Values = lists:filtermap(fun({_, T, V}) ->
+        case T =:= Type of
+            true -> {true, V};
+            _ -> false
+        end
+    end, Data),
+    mean(Values).
 
 
-% typ, dzien
-getDailyMean(_Arg0, _Arg1, _Arg2) ->
-    erlang:error(not_implemented).
+-spec getDailyMean(calendar:date(), kind(), monitor()) -> float() | undefined.
+getDailyMean(Date, Type, #monitor{name_to_station = NtS}) ->
+    Values = lists:flatmap(fun({_Name, #station{data = Data}}) ->
+        lists:filtermap(fun({{D, _}, T, V}) ->
+            case D =:= Date andalso T =:= Type of
+                true -> {true, V};
+                _ -> false
+            end
+        end, Data)
+    end, maps:to_list(NtS)),
+    mean(Values).
+
+-spec getAirQualityIndex(CoordOrName :: id(), Datetime :: timestamp(),
+    M :: monitor()) -> integer() | {error, no_data}.
+getAirQualityIndex(CoordOrName, Datetime,M) ->
+    {ok, #station{data = Data}} = findStation(CoordOrName, M),
+    Relevant = lists:filtermap(fun({D,T,V}) ->
+        case D =:= Datetime of
+            true -> {true, {T, V}};
+            false -> false
+        end
+    end, Data),
+    case lists:filtermap(fun({Type, Value}) ->
+        case overdose(Type, Value) of
+            undefined -> false;
+            Index -> {true, Index}
+        end
+    end, Relevant) of
+        [] -> {error, no_data};
+        Values -> lists:max(Values)
+    end.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+%% Checks if Name and Coordinates are not already registered in monitor
+-spec isStationAvailable(Name :: station_name(), Coord :: coord(), monitor()) ->
+    boolean().
+isStationAvailable(Name, Coord, M) ->
+    case {findStation({name, Name}, M), findStation({coord,Coord}, M)}  of
+        {{error, not_found}, {error, not_found}} -> true;
+        _ -> false
+    end.
 
 
 -spec findStation(id(), monitor()) -> {ok, station()} | {error, not_found}.
@@ -129,3 +177,24 @@ exists(Datetime, Kind, Dataset) ->
 spacetimeEquals({T, K, _}, {T, K, _}) -> true;
 spacetimeEquals(_, _) -> false.
 
+
+-spec mean([float()]) -> float() | undefined.
+mean([]) -> undefined;
+mean(List) -> lists:sum(List) / length(List).
+
+
+-spec getNorm(kind()) -> float() | undefined.
+getNorm(Type) ->
+    case Type of
+        "PM10" -> 50.0;
+        "PM2.5" -> 30.0;
+        "PM2,5" -> 30.0;
+        _ -> undefined
+    end.
+
+
+overdose(Type, Value) ->
+    case getNorm(Type) of
+        undefined -> undefined;
+        Norm -> round(Value * 100 / Norm)
+    end.
--- test/pollution_test.erl
+++ test/pollution_test.erl
@@ -25,12 +25,12 @@ create_monitor_test() ->
 
 addStation_test() ->
     M = pollution:createMonitor(),
-    ?assertMatch({ok, _}, pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M)).
+    ?assertMatch(#monitor{}, pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M)).
 
 addStation_fails_on_duplicate_test() ->
     % Setup
     M = pollution:createMonitor(),
-    {ok, M2} = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M),
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M),
 
     % Test
     ?assertMatch({error, _}, pollution:addStation(
@@ -39,14 +39,14 @@ addStation_fails_on_duplicate_test() ->
 
 addValue_test_() ->
     M1 = pollution:createMonitor(),
-    {ok, M2} = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
     M = M2,
     [
         % Station by name
-        ?_assertMatch({ok, #monitor{}}, pollution:addValue(
+        ?_assertMatch(#monitor{}, pollution:addValue(
             {name, ?STATION_NAME1}, ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, M)),
         % Station by coord name
-        ?_assertMatch({ok, #monitor{}}, pollution:addValue(
+        ?_assertMatch(#monitor{}, pollution:addValue(
             {coord, ?STATION_COORD1}, ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, M)),
         % Station by coord name and coord give equal result
         ?_assertEqual(
@@ -55,7 +55,7 @@ addValue_test_() ->
         ),
         % Created value can be found
         fun() ->
-            {ok, NewM} = pollution:addValue(
+            NewM = pollution:addValue(
                 {name, ?STATION_NAME1}, ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, M),
             ?assertEqual(?DATAPOINT1,
                 pollution:getOneValue({name, ?STATION_NAME1}, ?DATA_TIME1, ?DATA_TYPE1, NewM)
@@ -64,7 +64,7 @@ addValue_test_() ->
 
         %% Fail on duplicate
         {"addValue_fail_on_duplicate", fun() ->
-            {ok, NewM} = pollution:addValue(
+            NewM = pollution:addValue(
                 {name, ?STATION_NAME1}, ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, M),
             ?assertEqual({error, exists},
                 pollution:addValue({coord, ?STATION_COORD1}, ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, NewM))
@@ -82,8 +82,8 @@ addValue_test_() ->
 removeValue_test() ->
     % setup
     M1 = pollution:createMonitor(),
-    {ok, M2} = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
-    {ok, M3} = pollution:addValue({coord, ?STATION_COORD1},
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
+    M3 = pollution:addValue({coord, ?STATION_COORD1},
         ?DATA_TIME1, ?DATA_TYPE1, ?DATA_VALUE1, M2),
     M = M3,
 
@@ -94,19 +94,74 @@ removeValue_test() ->
     ?assertMatch({error, not_found}, pollution:getOneValue({coord, ?STATION_COORD1}, ?DATA_TIME1,
         ?DATA_TYPE1, NewM)).
 
-getStationMean_test() ->
+getStationMean_test_() ->
     [V1, V2, V3] = [2.0, 8.0, 16.5],
     Expected = (V1 + V2) / 2,
 
     M1 = pollution:createMonitor(),
-    {ok, M2} = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
-    {ok, M3} = pollution:addValue({coord, ?STATION_COORD1},
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
+    M3 = pollution:addValue({coord, ?STATION_COORD1},
         ?DATA_TIME1, ?DATA_TYPE1, V1, M2),
-    {ok, M4} = pollution:addValue({coord, ?STATION_COORD1},
+    M4 = pollution:addValue({coord, ?STATION_COORD1},
         ?DATA_TIME2, ?DATA_TYPE1, V2, M3),
-    {ok, M5} = pollution:addValue({coord, ?STATION_COORD1},
+    M5 = pollution:addValue({coord, ?STATION_COORD1},
         ?DATA_TIME2, ?DATA_TYPE2, V3, M4),
     M = M5,
 
-    ?assertEqual(Expected, pollution:getStationMean({coord, ?STATION_COORD1}, ?DATA_TYPE1, M)).
+    [
+        {"getStationMean_empty_test",
+        ?_assertEqual(undefined, pollution:getStationMean({coord, ?STATION_COORD1}, ?DATA_TYPE1, M2))},
+        ?_assertEqual(Expected, pollution:getStationMean({coord, ?STATION_COORD1}, ?DATA_TYPE1, M))
+    ].
+
+
+
+getDailyMean_test() ->
+    [V1, V2, V3] = Vals = [2.0, 8.0, 16.5],
+    Expected = lists:sum(Vals) / 3,
+    OtherTypeVal = 100.0,
+    OtherDayVal = 200.0,
+    Day = {2018, 04, 10},
+    OtherDay = {2018, 04, 11},
+    Time1 = {10, 42, 0},
+    Time2 = {11, 42, 0},
+
+    M1 = pollution:createMonitor(),
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
+    M3 = pollution:addStation(?STATION_NAME2, ?STATION_COORD2, M2),
+
+    M4 = pollution:addValue({coord, ?STATION_COORD1},
+        {Day, Time1}, ?DATA_TYPE1, V1, M3),
+    M5 = pollution:addValue({coord, ?STATION_COORD1},
+        {Day, Time2}, ?DATA_TYPE1, V2, M4),
+    M6 = pollution:addValue({coord, ?STATION_COORD2},
+        {Day, Time1}, ?DATA_TYPE1, V3, M5),
+
+    M7 = pollution:addValue({coord, ?STATION_COORD1},
+        {Day, Time1}, ?DATA_TYPE2, OtherTypeVal, M6),
+    M8 = pollution:addValue({coord, ?STATION_COORD1},
+        {OtherDay, Time1}, ?DATA_TYPE1, OtherDayVal, M7),
+
+    M = M8,
+
+    ?assertEqual(Expected, pollution:getDailyMean(Day, ?DATA_TYPE1, M)).
+
+
+getAirQualityIndex_test() ->
+    [PM10, PM25] = [75, 60],
+    Expected = 200,
+
+    M1 = pollution:createMonitor(),
+    M2 = pollution:addStation(?STATION_NAME1, ?STATION_COORD1, M1),
+    M3 = pollution:addValue({coord, ?STATION_COORD1},
+        ?DATA_TIME1, "PM10", PM10, M2),
+    M4 = pollution:addValue({coord, ?STATION_COORD1},
+        ?DATA_TIME1, "PM2,5", PM25, M3),
+    M5 = pollution:addValue({coord, ?STATION_COORD1},
+        ?DATA_TIME1, "Other,5", 5, M4),
+    M = M5,
+
+    ?assertEqual(Expected, pollution:getAirQualityIndex(
+        {coord, ?STATION_COORD1}, ?DATA_TIME1, M)).
+
 
